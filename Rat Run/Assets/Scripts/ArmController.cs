9a3cc17ef47cd47b670ed616a3825b01cfa9fab9
JLDooley <jldooley1@gmail.com>
2020-04-17 10:20:55 +0100

    ProBuilder imported. TestSpace created for mechanics implementation and testing.

---
+++ Rat Run/Assets/Scripts/ArmController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmController : MonoBehaviour
+{
+    public Rigidbody controller;
+    public Rigidbody arm;
+    public float xOff;
+    public float yOff;
+    public float zOff;
+    
+
+
+    void FixedUpdate()
+    {
+        
+        arm.MovePosition(new Vector3(controller.position.x + xOff, controller.position.y + yOff, controller.position.z + zOff));
+        
+        //transform.position = controller.position + new Vector3 (xOff, yOff, zOff);
+    }
+}
