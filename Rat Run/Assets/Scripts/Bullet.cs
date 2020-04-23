2120150fddc5771a118b0aedf30e4be5fd839ec3
JLDooley <jldooley1@gmail.com>
2020-04-23 22:45:59 +0100

    Work on grab mechanic.

---
+++ Rat Run/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 1.0f;
+    //private Transform aimTarget;
+    private Vector3 direction;
+
+    void Start()
+    {
+        direction = Vector3.forward;
+
+        Destroy(gameObject, 5.0f);
+    }
+
+    
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * direction.normalized;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Hit"); 
+        }
+        Destroy(gameObject);
+
+    }
+}
---
+++ Rat Run/Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.XR;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+
+
+public class BulletSpawner : MonoBehaviour
+{
+    public SteamVR_Action_Single inputAction;
+
+    public Hand hand;
+
+    public GameObject spawner;
+
+    public GameObject prefab;
+
+    public Interactable interactable;
+
+    private float timer = 0f;
+    
+    [Tooltip("Cooldown period (in seconds) between shots")]
+    public float rateOfFire = 1f;
+
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+        
+    }
+
+    void Update()
+    {
+        if (interactable.attachedToHand)
+        {
+            Debug.Log("Hand Detected");
+            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+
+            if (inputAction[source].active)
+            {
+                Shoot();
+            }
+        }
+
+        
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shoot() Run");
+        if (timer <= 0f)
+        {
+            Instantiate(prefab, spawner.transform);
+            timer = rateOfFire;
+        }
+        
+    }
+}
