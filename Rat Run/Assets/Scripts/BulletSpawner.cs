c6360b8cac0eb2673be8819ac0cc81aac985d66e
JLDooley <jldooley1@gmail.com>
2020-04-25 11:24:03 +0100

    Progress on shoot function

--- Rat Run/Assets/Scripts/BulletSpawner.cs
+++ Rat Run/Assets/Scripts/BulletSpawner.cs
@@ -5,55 +5,95 @@ using UnityEngine;
 using Valve.VR.InteractionSystem;
 using Valve.VR;
 
-
-
-public class BulletSpawner : MonoBehaviour
+namespace Valve.VR.InteractionSystem.Sample
 {
-    public SteamVR_Action_Single inputAction;
 
-    public Hand hand;
 
-    public GameObject spawner;
 
-    public GameObject prefab;
-
-    public Interactable interactable;
-
-    private float timer = 0f;
-    
-    [Tooltip("Cooldown period (in seconds) between shots")]
-    public float rateOfFire = 1f;
-
-    void Start()
+    public class BulletSpawner : MonoBehaviour
     {
-        interactable = GetComponent<Interactable>();
-        
-    }
+        public SteamVR_Action_Single inputAction;
 
-    void Update()
-    {
-        if (interactable.attachedToHand)
+        public SteamVR_Input_Sources source;
+
+        public float inputValue;
+        public float threshold = 0.75f;
+
+        public GameObject spawner;
+
+        public GameObject prefab;
+
+        public Interactable interactable;
+
+        private float timer = 0f;
+
+        [Tooltip("Cooldown period (in seconds) between shots")]
+        public float rateOfFire = 1f;
+
+
+/*        private void OnEnable()
         {
-            Debug.Log("Hand Detected");
-            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+            if (hand == null)
+                hand = this.GetComponent<Hand>();
 
-            if (inputAction[source].active)
+            if (inputAction == null)
             {
-                Shoot();
+                Debug.LogError("<b>[SteamVR Interaction]</b> No plant action assigned", this);
+                return;
+            }
+
+            
+
+            inputAction.AddOnChangeListener(OnInputActionChange, hand.handType);
+        }
+
+        private void OnInputActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue)
+        {
+            if (newValue)
+            {
+                Plant();
             }
         }
 
-        
-    }
-
-    void Shoot()
-    {
-        Debug.Log("Shoot() Run");
-        if (timer <= 0f)
+        private void OnDisable()
         {
-            Instantiate(prefab, spawner.transform);
-            timer = rateOfFire;
+            if (inputAction != null)
+                inputAction.RemoveOnChangeListener(OnInputActionChange, hand.handType);
         }
-        
+*/
+        void Start()
+        {
+            interactable = GetComponent<Interactable>();
+
+        }
+
+        void Update()
+        {
+            if (interactable.attachedToHand)
+            {
+                Debug.Log("Hand Detected");
+                source = interactable.attachedToHand.handType;
+                inputValue = inputAction.GetAxis(source);
+
+                if (inputValue > threshold)
+                {
+                    Debug.Log("Shooting");
+                    Shoot();
+                }
+            }
+
+
+        }
+
+        void Shoot()
+        {
+            Debug.Log("Shoot() Run");
+            if (timer <= 0f)
+            {
+                Instantiate(prefab, spawner.transform);
+                timer = rateOfFire;
+            }
+
+        }
     }
 }
