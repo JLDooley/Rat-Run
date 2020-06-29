016d012b8d985a3c0f3bca920c32be8ae90a97af
JLDooley <jldooley1@gmail.com>
2020-06-29 17:55:58 +0100

    Initial Aiming commit

---
+++ Rat Run/Assets/Scripts/ArmAim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAim : MonoBehaviour
+{
+    public Transform source;
+    public Transform crosshair;
+    public Transform debugTarget;
+    private Vector3 aimDirection;
+
+
+
+    
+    void Update()
+    {
+        aimDirection = (crosshair.position - source.position).normalized;
+        
+        RaycastHit hit;
+
+        if (Physics.Raycast(source.position, aimDirection, out hit, 100f))
+        {
+            Debug.DrawRay(source.position, aimDirection * hit.distance, Color.green);
+
+            debugTarget.position = hit.point;
+
+        }
+    }
+}
